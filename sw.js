bfb648f120757362bfb69bf0b3c4f50db3b64087
icyx19 <xiao.icy19@gmail.com>
2026-08-09 22:42:48 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'study-workbench-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './assets/index-qJA8eWpI.css',
+  './assets/index-CDgafLnx.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      if (response) return response;
+      return fetch(e.request).catch(() => {
+        if (e.request.destination === 'document') {
+          return caches.match('./index.html');
+        }
+      });
+    })
+  );
+});
